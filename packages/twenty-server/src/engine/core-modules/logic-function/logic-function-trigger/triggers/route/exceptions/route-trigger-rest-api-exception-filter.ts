37a0a933d1dce68bdb3cc0090e18680821900ace
Pedro Henrique Abreu <pedroabrev@MacBook-de-Pedro.local>
2026-07-28 21:37:55 -0300

    fix(auth): refresh expired app route tokens

--- packages/twenty-server/src/engine/core-modules/logic-function/logic-function-trigger/triggers/route/exceptions/route-trigger-rest-api-exception-filter.ts
+++ packages/twenty-server/src/engine/core-modules/logic-function/logic-function-trigger/triggers/route/exceptions/route-trigger-rest-api-exception-filter.ts
@@ -6,22 +6,32 @@ import {
 
 import type { Response } from 'express';
 
+import { AuthException } from 'src/engine/core-modules/auth/auth.exception';
+import { getAuthExceptionRestStatus } from 'src/engine/core-modules/auth/utils/get-auth-exception-rest-status.util';
+import { HttpExceptionHandlerService } from 'src/engine/core-modules/exception-handler/http-exception-handler.service';
 import {
   RouteTriggerException,
   RouteTriggerExceptionCode,
 } from 'src/engine/core-modules/logic-function/logic-function-trigger/triggers/route/exceptions/route-trigger.exception';
 import type { CustomException } from 'src/utils/custom-exception';
-import { HttpExceptionHandlerService } from 'src/engine/core-modules/exception-handler/http-exception-handler.service';
 
-@Catch(RouteTriggerException)
+@Catch(RouteTriggerException, AuthException)
 export class RouteTriggerRestApiExceptionFilter implements ExceptionFilter {
   constructor(
     private readonly httpExceptionHandlerService: HttpExceptionHandlerService,
   ) {}
 
-  catch(exception: RouteTriggerException, host: ArgumentsHost) {
+  catch(exception: RouteTriggerException | AuthException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+
+    if (exception instanceof AuthException) {
+      return this.httpExceptionHandlerService.handleError(
+        exception,
+        response,
+        getAuthExceptionRestStatus(exception),
+      );
+    }
 
     switch (exception.code) {
       case RouteTriggerExceptionCode.WORKSPACE_NOT_FOUND:
