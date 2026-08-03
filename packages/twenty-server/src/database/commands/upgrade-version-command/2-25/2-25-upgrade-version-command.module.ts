185c78219cdedc2dee3f450d008d671efdf48446
Pedro Henrique Abreu <pedroabrev@MacBook-de-Pedro.local>
2026-08-03 11:58:08 -0300

    feat(diex): add supervised metadata reparenting command

--- packages/twenty-server/src/database/commands/upgrade-version-command/2-25/2-25-upgrade-version-command.module.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/2-25/2-25-upgrade-version-command.module.ts
@@ -3,7 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { WorkspaceIteratorModule } from 'src/database/commands/command-runners/workspace-iterator.module';
 import { BackfillMessageListMembersJunctionTargetCommand } from 'src/database/commands/upgrade-version-command/2-25/2-25-workspace-command-1784567000000-backfill-message-list-members-junction-target.command';
+import { ReparentDiexMetadataCommand } from 'src/database/commands/upgrade-version-command/2-25/2-25-workspace-command-1785000000000-reparent-diex-metadata.command';
 import { FieldMetadataEntity } from 'src/engine/metadata-modules/field-metadata/field-metadata.entity';
+import { WorkspaceMetadataVersionModule } from 'src/engine/metadata-modules/workspace-metadata-version/workspace-metadata-version.module';
 import { WorkspaceCacheModule } from 'src/engine/workspace-cache/workspace-cache.module';
 import { WorkspaceMigrationRunnerModule } from 'src/engine/workspace-manager/workspace-migration/workspace-migration-runner/workspace-migration-runner.module';
 
@@ -13,7 +15,11 @@ import { WorkspaceMigrationRunnerModule } from 'src/engine/workspace-manager/wor
     WorkspaceCacheModule,
     WorkspaceMigrationRunnerModule,
     WorkspaceIteratorModule,
+    WorkspaceMetadataVersionModule,
   ],
-  providers: [BackfillMessageListMembersJunctionTargetCommand],
+  providers: [
+    BackfillMessageListMembersJunctionTargetCommand,
+    ReparentDiexMetadataCommand,
+  ],
 })
 export class V2_25_UpgradeVersionCommandModule {}
