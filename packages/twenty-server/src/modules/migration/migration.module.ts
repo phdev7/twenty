4694e35f4a3ebbf1e9cc323e53041b6a5eb916ca
Pedro Henrique Abreu <pedroabrev@MacBook-de-Pedro.local>
2026-08-05 19:04:00 -0300

    fix: provide token dependencies to migration controller

--- packages/twenty-server/src/modules/migration/migration.module.ts
+++ packages/twenty-server/src/modules/migration/migration.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { AuthModule } from 'src/engine/core-modules/auth/auth.module';
+import { TokenModule } from 'src/engine/core-modules/auth/token/token.module';
 import { KeyValuePairModule } from 'src/engine/core-modules/key-value-pair/key-value-pair.module';
 import { TwentyORMModule } from 'src/engine/twenty-orm/twenty-orm.module';
 import { WorkspaceCacheStorageModule } from 'src/engine/workspace-cache-storage/workspace-cache-storage.module';
@@ -8,7 +8,12 @@ import { MigrationController } from 'src/modules/migration/controllers/migration
 import { MigrationService } from 'src/modules/migration/services/migration.service';
 
 @Module({
-  imports: [AuthModule, KeyValuePairModule, TwentyORMModule, WorkspaceCacheStorageModule],
+  imports: [
+    TokenModule,
+    KeyValuePairModule,
+    TwentyORMModule,
+    WorkspaceCacheStorageModule,
+  ],
   controllers: [MigrationController],
   providers: [MigrationService],
   exports: [MigrationService],
